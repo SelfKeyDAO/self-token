ebf48c7da9002adf09cca3a16c33f7a76545e0bc
Andre Goncalves <andre@andregoncalves.com>
2023-05-25 18:40:37 +0100

    NEW: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# SELF token
+
+## Overview
+
+ERC20 Token contract with user initiated minting with signed authorization
+
+## Development
+
+All smart contracts are implemented in Solidity `^0.8.19`, using [Hardhat](https://hardhat.org/) as the Solidity development framework.
+
+### Prerequisites
+
+* [NodeJS](htps://nodejs.org), v16.1.0+
+* [Hardhat](https://hardhat.org/), which is a comprehensive framework for Ethereum development.
+
+### Initialization
+
+    npm install
+
+### Testing
+
+    npx hardhat test
+
---
+++ test/SelfTokenTests.js
@@ -0,0 +1,335 @@
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+function getTimestamp() {
+    const today = new Date();
+    const mseconds = today.getTime();
+    const seconds = Math.floor(mseconds / 1000);
+    const dateInSecs = Math.floor(new Date().getTime() / 1000);
+    return dateInSecs;
+}
+
+describe("SELF token Tests", function () {
+
+	let contract;
+    let authContract;
+
+    let owner;
+    let addr1;
+    let addr2;
+    let receiver;
+    let signer;
+    let addrs;
+
+    const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+    beforeEach(async function () {
+        [owner, addr1, addr2, receiver, signer, ...addrs] = await ethers.getSigners();
+
+        let authorizationContractFactory = await ethers.getContractFactory("SelfkeyIdAuthorization");
+        authContract = await authorizationContractFactory.deploy(signer.address);
+
+		let selfTokenContractFactory = await ethers.getContractFactory("SelfToken");
+        contract = await upgrades.deployProxy(selfTokenContractFactory, []);
+		await contract.deployed();
+
+        await contract.connect(owner).setAuthorizationContractAddress(authContract.address, { from: owner.address });
+
+    });
+
+    describe("Deployment", function() {
+        it("Deployed correctly and Selfkey.ID authorization contract was set", async function() {
+
+            expect(await contract.symbol()).to.equal('SELF');
+            expect(await contract.authorizationContractAddress()).to.equal(authContract.address);
+        });
+    });
+
+    describe("Governance functions", function() {
+        it("Owner should be able to mint", async function() {
+            const _amount = 100;
+            await expect(contract.connect(owner).mint(addr1.address, _amount, { from: owner.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(ZERO_ADDRESS, addr1.address, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+        });
+
+        it("Non-owner should not be able to mint", async function() {
+            const _amount = 100;
+            await expect(contract.connect(addr1).mint(addr1.address, _amount, { from: addr1.address }))
+                .to.be.revertedWith('Ownable: caller is not the owner');
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+        });
+
+        it("Owner can pause", async function() {
+            expect(await contract.paused()).to.equal(false);
+
+            await expect(contract.connect(owner).pause({ from: owner.address }))
+                .to.emit(contract, 'Paused')
+
+            expect(await contract.paused()).to.equal(true);
+        });
+
+        it("Owner can pause", async function() {
+            expect(await contract.paused()).to.equal(false);
+
+            await expect(contract.connect(owner).pause({ from: owner.address }))
+                .to.emit(contract, 'Paused')
+
+            expect(await contract.paused()).to.equal(true);
+        });
+
+        it("When paused owner cannot mint", async function() {
+            expect(await contract.paused()).to.equal(false);
+
+            await expect(contract.connect(owner).pause({ from: owner.address }))
+                .to.emit(contract, 'Paused')
+
+            expect(await contract.paused()).to.equal(true);
+
+            const _amount = 100;
+            await expect(contract.connect(owner).mint(addr1.address, _amount, { from: owner.address }))
+                .to.be.revertedWith('Pausable: paused');
+        });
+
+    })
+
+    describe("Authorized minting", function() {
+        let expiration;
+        it("Should create a deadline in seconds based on last block timestamp", async function () {
+            const provider = ethers.getDefaultProvider()
+            const lastBlockNumber = await provider.getBlockNumber()
+            const lastBlock = await provider.getBlock(lastBlockNumber)
+            expiration = lastBlock.timestamp;
+        });
+
+        it("Should be able to mint SELF if authorized", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(ZERO_ADDRESS, addr1.address, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+        });
+
+        it("Should be able to mint SELF if authorized only once", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(ZERO_ADDRESS, addr1.address, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.be.revertedWith('Payload already used');
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+        });
+
+        it("Should NOT be able to mint SELF if authorized but asked for wrong amount", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(_to, 100, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.be.revertedWith('Verification failed');
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+        });
+
+        it("Should NOT be able to mint SELF if authorized but asked for wrong wallet address", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(addr2.address, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.be.revertedWith("Invalid subject");
+
+            expect(await contract.balanceOf(addr2.address)).to.equal(0);
+        });
+
+        it("Should NOT be able to mint SELF if authorized for different scope", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(addr1.address, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.be.revertedWith("Verification failed");
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+        });
+
+        it("Should NOT be able to mint SELF if authorized from an untrusted signer", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await addr2.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, addr2.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(addr1.address, _amount, _timestamp, addr2.address, signature, { from: addr1.address }))
+                .to.be.revertedWith("Invalid signer");
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+        });
+
+        it("Should NOT be able to mint SELF if authorized more than 4 hours ago", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await addr2.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, addr2.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(addr1.address, _amount, _timestamp, addr2.address, signature, { from: addr1.address }))
+                .to.be.revertedWith("Invalid signer");
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+        });
+
+        it("Should be able to burn own SELF", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(ZERO_ADDRESS, addr1.address, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.be.revertedWith('Payload already used');
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+
+            await expect(contract.connect(addr1).burn(_amount, { from: addr1.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(addr1.address, ZERO_ADDRESS, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+        });
+
+        it("Should not be able to burn others SELF", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(ZERO_ADDRESS, addr1.address, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.be.revertedWith('Payload already used');
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+
+            await expect(contract.connect(addr2).burnFrom(addr1.address, _amount, { from: addr2.address }))
+                .to.be.revertedWith('ERC20: insufficient allowance');
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+        });
+
+        it("Should be able to burn others SELF with allowance", async function() {
+            const _from = contract.address;
+            const _to = addr1.address;
+            const _amount = 10;
+            const _scope = 'mint:self';
+            const _timestamp = expiration;
+
+            const hash = await authContract.getMessageHash(_from, _to, _amount, _scope, _timestamp);
+            const signature = await signer.signMessage(ethers.utils.arrayify(hash));
+
+            expect(await authContract.verify(_from, _to, _amount, _scope, _timestamp, signer.address, signature)).to.equal(true);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(ZERO_ADDRESS, addr1.address, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+
+            await expect(contract.connect(addr1).self_mint(_to, _amount, _timestamp, signer.address, signature, { from: addr1.address }))
+                .to.be.revertedWith('Payload already used');
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(_amount);
+
+            await expect(contract.connect(addr1).approve(addr2.address, _amount), { from: addr1.address })
+                .to.emit(contract, 'Approval')
+                .withArgs(addr1.address, addr2.address, _amount);
+
+            await expect(contract.connect(addr2).burnFrom(addr1.address, _amount, { from: addr2.address }))
+                .to.emit(contract, 'Transfer')
+                .withArgs(addr1.address, ZERO_ADDRESS, _amount);
+
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+        });
+
+    });
+});
